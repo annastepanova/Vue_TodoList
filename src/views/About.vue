c2a912320a22830d3bfb4d73a390e1b94ccd176c
annastepanova <annastepanova1811@gmail.com>
2020-05-08 14:10:36 -0400

    added routes

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app built with Vue.js.</p>
+  </div>
+</template>
+
+<style scoped>
+ 
+ .about {
+   margin: 20px 20px;
+ }
+
+</style>
